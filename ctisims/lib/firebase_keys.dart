f0b2c5fd91b12b4382be731b0140b6813e5f3f8f
armankurt <armanyilmazkurt@gmail.com>
2025-05-12 22:29:45 +0300

    Updated Firebase configuration with the new project details

---
+++ ctisims/lib/firebase_keys.dart
@@ -0,0 +1,17 @@
+// Web configuration
+const String firebaseApiKey = "AIzaSyBYJEa8RojFIWrCvg390U6WF8crdOuwq90";
+const String firebaseAuthDomain = "ctis-internship-86a7f.firebaseapp.com";
+const String firebaseProjectId = "ctis-internship-86a7f";
+const String firebaseStorageBucket =
+    "ctis-internship-86a7f.firebasestorage.app";
+const String firebaseMessagingSenderId = "657303859821";
+const String firebaseAppId = "1:657303859821:web:6a700c1903782c24c20564";
+const String firebaseMeasurementId = "G-WQELMRF8NW";
+
+// iOS configuration
+const String firebaseIosApiKey = "AIzaSyCD5cOs3kCAbaIib45c212uGzdwJtxVCeU";
+const String firebaseIosAppId = "1:933212537172:ios:5f8a3946ad5d790e9708c1";
+const String firebaseIosMessagingSenderId = "933212537172";
+const String firebaseIosProjectId = "ctisims-5e282";
+const String firebaseIosStorageBucket = "ctisims-5e282.firebasestorage.app";
+const String firebaseIosBundleId = "com.example.ctisims";
